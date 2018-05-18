d7b8544fab2cbb27c37fdb3de6073706a02c2882
Dzengiz Tafa <dzengiz.tafa@telenet.be>
2018-05-18 16:58:39 +0200

    Added base layout for the detailsActivity holding all the textviews and edittexts. FABs for save option and order option still need to be added

--- app/src/main/java/com/example/android/inventoryapp/UI/CatalogActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/UI/CatalogActivity.java
@@ -1,7 +1,22 @@
 package com.example.android.inventoryapp.UI;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.android.inventoryapp.R;
 
@@ -11,5 +26,16 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        // Setup FAB to open the DetailsActivity
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        CatalogActivity.this, DetailsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
